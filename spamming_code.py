26d227f4db849f362c5a08911a25803581beea1d
Thiago Augusto <thicofaria@outlook.com>
2023-08-30 19:08:40 -0300

    entering the first version of the code

---
+++ spamming_code.py
@@ -0,0 +1,25 @@
+import pyautogui as pag
+import time
+
+vezes_para_mandar = int(0)
+def localizacao():
+    input('when you press enter, the program will count until 10, you have 10 seconds to put your mouse on the writing bar of wherever you want to spam a messege. please leave it there for 10 seconds so we can capture it:')
+
+    time.sleep(10)
+    posição = pag.position()
+    return posição
+
+
+
+def spama(posição, vezes_para_mandar):
+    pag.write('spamming')
+    pag.press('enter')
+
+
+posição = localizacao()
+
+pag.click(posição)
+
+while vezes_para_mandar <= 10:
+    spama(posição,vezes_para_mandar)
+    vezes_para_mandar += 1
