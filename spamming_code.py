96e913593593213dfac5d99a7dfbd89fe86a551d
Thiago Augusto <thicofaria@outlook.com>
2023-08-30 19:26:45 -0300

    adicionando a funcao de colocar quantas vezes spammar alguem

--- spamming_code.py
+++ spamming_code.py
@@ -1,7 +1,8 @@
 import pyautogui as pag
 import time
 
-vezes_para_mandar = int(0)
+vezes_mandadas = int(0)
+
 def localizacao():
     input('when you press enter, the program will count until 10, you have 10 seconds to put your mouse on the writing bar of wherever you want to spam a messege. please leave it there for 10 seconds so we can capture it:')
 
@@ -19,12 +20,16 @@ def definir_mensagem():
     mensagem = input('write the message that you want to spam:')
     return mensagem
 
+def definir_vezes():
+    vezes = int(input('write the number of times that you want to spam the message:')) - 1
+    return vezes
 
 mensagem = definir_mensagem()
+vezes = definir_vezes()
 posição = localizacao()
 
 pag.click(posição)
 
-while vezes_para_mandar <= 10:
-    spama(posição,vezes_para_mandar,mensagem)
-    vezes_para_mandar += 1
+while vezes_mandadas <= vezes:
+    spama(posição,vezes_mandadas,mensagem)
+    vezes_mandadas += 1
